d98b972e952e448265f475c8ec76823dd1c6a395
Cloud617 <Aronwong95@gmail.com>
2015-12-16 16:19:35 -0600

    I created my admin user to log on to the admin
    site and checked my server by running the server.I
    entered the admin site and modified the poll app by changing
    my polls/admin file.I also customized my "What's up" question using
    different polls/admin codes.

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+# from django.contrib import admin
+from django.contrib import admin
+
+from .models import Question
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
