10e7ac08981f647dcd8c68ae9178db96bd032f78
Cloud617 <Aronwong95@gmail.com>
2015-12-18 13:10:13 -0600

    I upadete the polls/admin.py
    I add the question and chocies in
    the Polls/admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
-# from django.contrib import admin
-from django.contrib import admin
+from .models import Choice, Question
 
-from .models import Question
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -12,5 +13,6 @@ class QuestionAdmin(admin.ModelAdmin):
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
